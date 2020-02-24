2a13e7fda92a0a8b355ebc5e22d6431324c88be3
Kefa Mutuma <mutmakeffa@gmail.com>
2020-02-24 20:22:44 +0300

    configured DATABASE_URL using mongoDB atlas

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const mongoose = require('mongoose')
 
 const PORT = process.env.PORT
 
-mongoose.connect(PORT, {
+mongoose.connect(process.env.DATABASE_URL, {
      useNewUrlParser: true , 
      useUnifiedTopology: true}
 )
